e4e249d7692feaba6ae15e8e7d32280516f54f86
David Chu <davidvanchu@gmail.com>
2018-11-07 23:04:29 -0500

    Updates

---
+++ precache-manifest.7c63ba7592bf881723addf67b966d606.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "730718e76a96ae9f45aa",
+    "url": "/static/css/main.0aa62b60.chunk.css"
+  },
+  {
+    "revision": "730718e76a96ae9f45aa",
+    "url": "/static/js/main.730718e7.chunk.js"
+  },
+  {
+    "revision": "252e1990f01ba48f0700",
+    "url": "/static/js/1.252e1990.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a724146a67adde8efd2c0ae5be219c03",
+    "url": "/index.html"
+  }
+];
